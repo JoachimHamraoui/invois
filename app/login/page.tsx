5b2340281ce6f537c52aa74addb318fdfd3e6bfd
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-08-20 19:50:20 +0200

    config: configured mailtrap and nodemailer for auth through magic link by email

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "../utils/auth";
 
 export default function Login() {
     return (
@@ -13,7 +14,12 @@ export default function Login() {
                         <CardDescription>Enter your email below to login to your account.</CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <form className="flex flex-col gap-y-4">
+                        <form action={
+                            async() => {
+                                "use server";
+                                await signIn()
+                            }
+                        } className="flex flex-col gap-y-4">
                             <div className="flex flex-col gap-y-4">
                                 <Label htmlFor="email">Email</Label>
                                 <Input id="email" type="email" placeholder="Email" />
