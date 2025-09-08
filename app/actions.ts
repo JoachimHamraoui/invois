c91e7807a72f42a946126cd50f24495c6de1a877
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-09-08 08:20:45 +0200

    feat(onboarding): clientside and serverside form validation with zod

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,11 @@
 "use server";
-
+// use Zod V3.25.0
 import { requireUser } from "./utils/hooks";
 import { parseWithZod } from "@conform-to/zod";
 import { onboardingSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 
-export async function onboardUser(formData: FormData) {
+export async function onboardUser(previousState: any, formData: FormData) {
   const session = await requireUser();
 
   const submission = parseWithZod(formData, { schema: onboardingSchema });
--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -8,8 +10,22 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SubmitButton } from "../components/SubmitButtons";
+import { useActionState } from "react";
+import { onboardUser } from "../actions";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { onboardingSchema } from "../utils/zodSchemas";
 
 export default function Onboarding() {
+  const [lastResult, action] = useActionState(onboardUser, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: onboardingSchema });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
   return (
     <div className="h-screen w-full flex items-center justify-center">
       <Card className="w-[480px]">
@@ -20,22 +36,38 @@ export default function Onboarding() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="grid gap-4">
+          <form
+            className="grid gap-4"
+            action={action}
+            id={form.id}
+            onSubmit={form.onSubmit}
+            noValidate
+          >
             <div className="grid grid-cols-2 gap-4">
               <div className="flex flex-col gap-2">
-                <Label >First Name</Label>
-                <Input type="text" name="firstName" placeholder="John" />
+                <Label>First Name</Label>
+                <Input
+                  type="text"
+                  name={fields.firstName.name}
+                  key={fields.firstName.key}
+                  defaultValue={fields.firstName.initialValue}
+                  placeholder="John"
+                />
               </div>
               <div className="flex flex-col gap-2">
                 <Label>Last Name</Label>
-                <Input type="text" name="lastName" placeholder="Doe" />
+                <Input type="text" placeholder="Doe" />
               </div>
             </div>
             <div className="flex flex-col gap-2">
-                <Label>Address</Label>
-                <Input type="text" name="lastName" placeholder="Berlin Ave. 123" />
-              </div>
-              <SubmitButton text="Finish onboarding" />
+              <Label>Address</Label>
+              <Input
+                type="text"
+                name="lastName"
+                placeholder="Berlin Ave. 123"
+              />
+            </div>
+            <SubmitButton text="Finish onboarding" />
           </form>
         </CardContent>
       </Card>
