dd752167df64c6dd60deeb6b7af54a555593043c
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-16 21:25:12 +0200

    feat(edit): edit invoice page now has data of selected invoice as default value

--- app/dashboard/invoices/[invoiceId]/page.tsx
+++ app/dashboard/invoices/[invoiceId]/page.tsx
@@ -1,3 +1,4 @@
+import { EditInvoice } from "@/app/components/EditInvoice";
 import { prisma } from "@/app/utils/db";
 import { requireUser } from "@/app/utils/hooks";
 import { notFound } from "next/navigation";
@@ -25,8 +26,6 @@ export default async function EditInvoiceRoute({ params }: { params: Params }) {
   const data = await getData(invoiceId, session.user?.id as string);
 
   return (
-    <div>
-      <h1>Edit Invoice</h1>
-    </div>
+    <EditInvoice data={data} />
   );
 }
