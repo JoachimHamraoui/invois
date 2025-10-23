702fa3c0ebadd34e7c843cea65819dd8eb1650ea
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-23 16:57:11 +0200

    feat(delete): page for deletion of invoice

--- app/dashboard/invoices/[invoiceId]/delete/page.tsx
+++ app/dashboard/invoices/[invoiceId]/delete/page.tsx
@@ -1,4 +1,11 @@
+import { SubmitButton } from "@/app/components/SubmitButtons";
 import { prisma } from "@/app/utils/db";
+import { requireUser } from "@/app/utils/hooks";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Invoice } from "@/lib/generated/prisma";
+import { CrossIcon, Delete, DeleteIcon, FileWarning } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 async function Authorize(invoiceId: string, userId: string) {
@@ -14,10 +21,30 @@ async function Authorize(invoiceId: string, userId: string) {
     }
 }
 
-export default function DeleteInvoiceRoute() {
+type Params = Promise<{invoiceId: string}>
+
+export default async function DeleteInvoiceRoute({ params }: { params: Params }) {
+    const { invoiceId } = await params;
+    const session = await requireUser();
+
+    await Authorize(invoiceId, session.user?.id as string);
     return (
-      <div>
-        <h1>Delete Invoice</h1>
+      <div className="flex flex-1 justify-center items-center">
+        <Card className="w-[500px]">
+            <CardHeader>
+                <CardTitle>Delete Invoice</CardTitle>
+                <CardDescription>Are you sure you want to delete this invoice ?</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <FileWarning className="text-red-300 mx-auto size-24 my-10" />
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Link className={buttonVariants({ variant: "outline" })} href="/dashboard/invoices">Cancel</Link>
+                <form>
+                    <SubmitButton text="Delete Invoice" />
+                </form>
+            </CardFooter>
+        </Card>
       </div>
     );
 }
