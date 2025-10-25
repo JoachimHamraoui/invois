02d64564dac630da33f2dc09c5f5209d133e40c0
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-25 19:50:23 +0200

    feat(paid): front-end UI for "Mark as Paid" funciton is ready.

--- app/dashboard/invoices/[invoiceId]/paid/page.tsx
+++ app/dashboard/invoices/[invoiceId]/paid/page.tsx
@@ -1,7 +1,27 @@
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Verified } from "lucide-react";
+import Link from "next/link";
+
 export default function MarkAsPaid() {
     return (
-        <div>
-            <h1>Mark as Paid</h1>
+        <div className="flex flex-1 justify-center items-center">
+            <Card className="w-[500px]">
+                <CardHeader>
+                    <CardTitle>Mark as paid ?</CardTitle>
+                    <CardDescription>Are you sure you want to mark this invoice as paid ?</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Verified className="text-green-300 mx-auto size-24 my-10" />
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Link className={buttonVariants({ variant: "outline" })} href="/dashboard/invoices">Cancel</Link>
+                    <form>
+                        <SubmitButton text="Mark as paid" variant="default" />
+                    </form>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
