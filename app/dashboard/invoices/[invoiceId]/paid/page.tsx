fa1104033014c2f2d11487751e0c45f76cfa905e
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-25 20:10:49 +0200

    feat(paid): invoice can now be set to paid using the "Mark as paid" option in the dropdown

--- app/dashboard/invoices/[invoiceId]/paid/page.tsx
+++ app/dashboard/invoices/[invoiceId]/paid/page.tsx
@@ -1,16 +1,32 @@
 import { MarkAsPaidAction } from "@/app/actions";
 import { SubmitButton } from "@/app/components/SubmitButtons";
+import { prisma } from "@/app/utils/db";
+import { requireUser } from "@/app/utils/hooks";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Verified } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
+async function Authorize(invoiceId: string, userId: string) {
+    const data = await prisma.invoice.findUnique({
+        where: {
+            id: invoiceId,
+            userId: userId
+        },
+    })
 
+    if (!data) {
+        return redirect("/dashboard/invoices")
+    }
+}
 
-type Params = Promise<{invoice: string}>
+type Params = Promise<{invoiceId: string}>
 
  export default async function MarkAsPaid({ params }: { params: Params }) {
-    const { invoice } = await params
+    const { invoiceId } = await params;
+    const session = await requireUser();
+    await Authorize(invoiceId, session.user?.id as string);
     return (
         <div className="flex flex-1 justify-center items-center">
             <Card className="w-[500px]">
@@ -25,7 +41,7 @@ type Params = Promise<{invoice: string}>
                     <Link className={buttonVariants({ variant: "outline" })} href="/dashboard/invoices">Cancel</Link>
                     <form action={async () => {
                         "use server"
-                        await MarkAsPaidAction(invoice)
+                        await MarkAsPaidAction(invoiceId)
                     }}>
                         <SubmitButton text="Mark as paid" variant="default" />
                     </form>
