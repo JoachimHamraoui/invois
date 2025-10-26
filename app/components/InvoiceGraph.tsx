cb491ff944ac1c3bd4b0887cf20b80bf16b430e1
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-26 17:36:37 +0100

    feat(dashboard): fetching data for graph and formatting (WIP)

--- app/components/InvoiceGraph.tsx
+++ app/components/InvoiceGraph.tsx
@@ -1,15 +1,58 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Graph } from "./Graph";
+import { prisma } from "../utils/db";
+
+async function getInvoices(userId: string) {
+  const rawData = await prisma.invoice.findMany({
+    where: {
+      status: "PAID",
+      userId: userId,
+      createdAt: {
+        lte: new Date(),
+        gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+      },
+    },
+    select: {
+      createdAt: true,
+      total: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  // Group and aggregate data
+
+  const aggregatedData = rawData.reduce((acc: { [key: string]: number }, curr) => {
+    const date = new Date(curr.createdAt).toLocaleDateString("fr-BE", {
+        month: "short",
+        day: "numeric",
+    });
+    acc[date] = (acc[date] || 0) + curr.total;
+
+    return acc;
+  }, {});
+
+}
 
 export function InvoiceGraph() {
-    return (
-        <Card className="lg:col-span-2">
-            <CardHeader>
-                <CardTitle>Paid Invoices</CardTitle>
-                <CardDescription>Invoices which have been paid in the last 30 days.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card className="lg:col-span-2">
+      <CardHeader>
+        <CardTitle>Paid Invoices</CardTitle>
+        <CardDescription>
+          Invoices which have been paid in the last 30 days.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Graph />
+      </CardContent>
+    </Card>
+  );
 }
