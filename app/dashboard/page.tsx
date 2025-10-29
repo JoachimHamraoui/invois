5bf9b557766233224302332a26c73c5291f74ca8
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-10-29 17:51:57 +0100

    feat(dashboard): ternary operator in case no invoices have been made yet

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,18 +2,39 @@ import { DashboardBlocks } from "../components/DashboardBlocks";
 import { InvoiceGraph } from "../components/InvoiceGraph";
 import { RecentInvoices } from "../components/RecentInvoices";
 import { signOut } from "../utils/auth";
+import { prisma } from "../utils/db";
 import { requireUser } from "../utils/hooks";
+
+async function getData(userId: string) {
+  const data = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return data;
+}
 
 export default async function DashboardRoute() {
   const session = await requireUser();
+  const data = await getData(session.user?.id as string);
 
   return (
     <>
-      <DashboardBlocks />
-      <div className="grid gap-4 lg:grid-cols-3">
-        <InvoiceGraph />
-        <RecentInvoices />
-      </div>
+      {data.length < 1 ? (
+        <p>No data available</p>
+      ) : (
+        <>
+          <DashboardBlocks />
+          <div className="grid gap-4 lg:grid-cols-3">
+            <InvoiceGraph />
+            <RecentInvoices />
+          </div>
+        </>
+      )}
     </>
   );
 }
